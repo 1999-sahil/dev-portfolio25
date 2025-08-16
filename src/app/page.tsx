21f64f66e1ab6566de846c854d1419bba642516a
Sahil Ahmed <sahilahmed466@gmail.com>
2025-08-16 23:03:49 +0530

    feat: add dark mode support with theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
-      <h2 className="">Home Page</h2>
+      <h2 className="font-inter font-bold">Home Page (inter)</h2>
+      <h2 className="font-poppins font-bold">Home Page (poppins)</h2>
+      <h2 className="font-openSans font-bold">Home Page (open_sans)</h2>
+
+      <ModeToggle />
     </div>
   );
 }
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  // Avoid hydration mismatch in Next.js
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+
+  return (
+    <button
+      onClick={toggleTheme}
+      title="Toggle theme"
+      className="inline-flex items-center justify-center gap-2 rounded-md size-8 transition-colors hover:bg-accent hover:text-accent-foreground"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="size-4.5"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+        <path d="M12 3l0 18"></path>
+        <path d="M12 9l4.65 -4.65"></path>
+        <path d="M12 14.3l7.37 -7.37"></path>
+        <path d="M12 19.6l8.85 -8.85"></path>
+      </svg>
+      <span className="sr-only">Toggle theme</span>
+    </button>
+  );
+}
